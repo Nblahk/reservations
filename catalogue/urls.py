7fa3239828cc70f24f962f4dd1ec47bdfdeb47f0
Nblahk <ahakeu.nabil@hotmail.fr>
2025-05-01 19:35:57 +0200

    Update modèle User +migration +données initiales

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,20 +1,12 @@
-"""reservations.catalogue URL Configuration
-"""
 from django.urls import path
-
 from . import views
 
-from django.contrib import admin
-from .models import Artist
-
-admin.site.register(Artist)
-
-app_name='catalogue'
+app_name = 'catalogue'
 
 urlpatterns = [
-    path('artist/', views.artist.index, name='artist-index'),
-    path('artist/<int:artist_id>', views.artist.show, name='artist-show'),
-    path('artist/edit/<int:artist_id>', views.artist.edit, name='artist-edit'),
-    path('artist/create', views.artist.create, name='artist-create'),
-    path('artist/<int:artist_id>/delete/', views.artist.delete, name='artist-delete'),
+    path('artist/',                       views.artist.index,   name='artist_index'),
+    path('artist/create/',                views.artist.create,  name='artist_create'),
+    path('artist/<int:artist_id>/',       views.artist.show,    name='artist_show'),
+    path('artist/<int:artist_id>/edit/',  views.artist.edit,    name='artist_edit'),
+    path('artist/<int:artist_id>/delete/',views.artist.delete,  name='artist_delete'),
 ]
